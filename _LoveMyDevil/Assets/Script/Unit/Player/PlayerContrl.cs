f48bbc55722dd2f9a46f6e188dbfd5e98863d87b
Dead-DogBird <48205134+Dead-DogBird@users.noreply.github.com>
2023-09-15 14:35:04 +0900

    230915

---
+++ _LoveMyDevil/Assets/Script/Unit/Player/PlayerContrl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContrl : MonoBehaviour
+{
+    public struct UserInput
+    {
+        float _nowAxisState;
+        bool _nowSpace;
+        bool _leftMouseButton;
+        private Vector3 _mousePos;
+        private bool _skillKey;
+        public bool SpaceState => _nowSpace;
+        public bool LeftMouseState => _leftMouseButton;
+        public float AxisState => _nowAxisState;
+        public bool SkillKey => _skillKey;
+        public Vector3 MousePos => _mousePos;
+        public void InputUpdate()
+        {
+            _nowAxisState = Input.GetAxisRaw("Horizontal");
+            _nowSpace = Input.GetKeyDown(KeyCode.Space);
+            _leftMouseButton = Input.GetMouseButton(0);
+            _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+    public UserInput Userinput;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Userinput.InputUpdate();
+    }
+}
---
+++ _LoveMyDevil/Assets/Script/Unit/Player/PlayerMove.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // 컴포넌트들
+    public Rigidbody2D _playerRigidbody { get; private set; }
+    private PlayerContrl _playerControll;
+
+    //플레이어 스탯 값
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 1000;
+
+    /// <summary>
+    /// 최대 점프 횟수
+    /// </summary>
+    [SerializeField] private int MAXJUMP = 2;
+
+    //기타 트리거들
+    private bool _isjumping;
+    private int jumpCount = 0;
+
+    void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+        _playerControll = GetComponent<PlayerContrl>();
+
+    }
+
+    void Update()
+    {
+        Jump();
+        transform.Translate(new Vector3(speed * _playerControll.Userinput.AxisState, 0) * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (jumpCount >= MAXJUMP) return;
+        if (!_playerControll.Userinput.SpaceState) return;
+        _isjumping = true;
+        jumpCount++;
+
+        if (jumpCount == 2)
+        {
+            _playerRigidbody.velocity = Vector2.zero;
+            _playerRigidbody.AddForce(new Vector2(0, jumpForce));
+            return;
+        }
+        _playerRigidbody.AddForce(new Vector2(0, jumpForce));
+        _playerRigidbody.velocity = Vector2.zero;
+
+
+    }
+
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if ((other.gameObject.CompareTag("Ground")||
+            other.gameObject.CompareTag("ColoredPlatform")||
+            other.gameObject.CompareTag("DropPlatform")) && other.contacts[1].normal.y > 0.7f)
+        {
+            _isjumping = false;
+            jumpCount = 0;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if ((other.gameObject.CompareTag("Ground")||
+            other.gameObject.CompareTag("ColoredPlatform") ||
+            other.gameObject.CompareTag("DropPlatform")) && !_isjumping)
+        {
+            _isjumping = true;
+            jumpCount = 1;
+        }
+    }
+
+
+
+
+}
